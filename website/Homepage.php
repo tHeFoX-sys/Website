32677c9b5390914c4a3b328323204dbd5be5b545
THe_FøX <aristeidisstagkos21203@gmail.com>
2025-06-27 01:15:13 +0300

    Add files via upload

---
+++ website/Homepage.php
@@ -0,0 +1,9 @@
+<?php
+
+include("Login.html");
+
+$password = $_POST["password"];
+
+echo "I HAVE YOUR PASSWORD {$password} <br>";
+echo "Imagine an XSS with PHP...me laughing in the background";
+
